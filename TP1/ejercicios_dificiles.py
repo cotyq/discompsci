7bc0676cc060b464b44d1054fcee61359004e238
cotyq <cotii.q@gmail.com>
2020-09-16 16:42:07 -0300

    Commit inicial TP1. Se agregan las declaraciones de las funciones

---
+++ TP1/ejercicios_dificiles.py
@@ -0,0 +1,14 @@
+def invitados(lista):
+    """
+    Imaginen que poseemos un diccionario de la forma nombre -> estado (clave -> valor),
+    el estado representa si la persona cuyo nombre es nombre irá o no a tu cumpleaños
+    """
+    pass
+
+
+def justificar(palabra):
+    """
+    Dado un string implementar la función justificar que fija la longitud de cada línea en 80 caracteres
+     y justifica cada línea.
+     """
+    pass
---
+++ TP1/strings.py
@@ -0,0 +1,37 @@
+def facturas(cantidad):
+    """
+    Dado una número de facturas, implementar una función que devuleva el string
+    Cantidad de facturas: <nro> donde nro es el número que se pasa como argumento.
+    Si las facturas son mas de 12, se tiene que devolver Cantidad de facturas: muchas.
+    """
+    pass
+
+
+def ambos(w):
+    """
+    Dado un string s, implementar la función ambos que devuelve un string
+    construido con los dos primeros y dos últimos caracteres. Por ejemplo,
+    aplicar ambos a primavera devuelve prra. Si s posee menos de dos caracteres,
+    el resultado es el string vacio.
+    """
+    pass
+
+
+def fix(uwu):
+    """
+    Dado un string s, implementar una función fix que reemplaza todas las
+    ocurrencias del primer caracter por * a excepción de la primera ocurrencia.
+    Por ejemplo evaluar fix a la palabra burbuja devuelve bur*uja.
+    Ayuda, estudiar la función replace.
+    """
+    pass
+
+
+def mezclar(a, b):
+    """
+    Dados dos strings a y b, implementar la función mezclar que devuleve el
+    string a y b separados por un espacio, excepto los primeros caracteres
+    de cada string que son intercambiados. Por ejemplo, mezclar('mix' , 'pod')
+    devuelve pix mod.
+    """
+    pass
