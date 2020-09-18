2c181a29d5f6b2afe352e035b26ed5db29a3dc2f
eugeborzone <eugenioborzone@gmail.com>
2020-09-18 18:50:52 -0300

    Terminado TP2

---
+++ TP2/Introspeccion.py
@@ -0,0 +1,39 @@
+"""
+6. Introspección
+Implementar la función evaluar que tome un objeto y un string como parámetros y si el string es un atributo, que
+devuelva el valor del atributo.
+
+Si es un método, devolver el resultado de aplicar el método al objeto (usar parámetros opcionales en evaluar para
+parámetros de métodos con parámetros) y si no es un método, ni un atributo, lanzar una Exception del tipo
+UnkonowTypeError que es-un TypeError el cual recibe comp parámetro el nombre del atributo desconocido.
+"""
+
+
+class UnkonowTypeError(TypeError):
+    def __init__(self, str):
+        super().__init__(str)
+
+
+def introspeccion(objeto, fun_or_attr, *args, **kwargs):
+    if hasattr(objeto, fun_or_attr) and not callable(getattr(objeto, fun_or_attr)):
+        return getattr(objeto, fun_or_attr)
+    if callable(getattr(objeto, fun_or_attr)):
+        return getattr(objeto, fun_or_attr)(*args, **kwargs)
+    raise UnkonowTypeError(fun_or_attr)
+
+
+class Fruta:
+    def __init__(self, nombre, color):
+        self.nombre = nombre
+        self.color = color
+
+    def mensaje(self, texto='El contenido es: '):
+        print(texto + self.nombre)
+
+
+if __name__ == "__main__":
+    manzana = Fruta('manzana', 'roja')
+    introspeccion(manzana, 'nombre')
+    introspeccion(manzana, 'mensaje')
+    introspeccion(manzana, 'mensaje', 'El mensaje modificado es: ')
+    introspeccion(manzana, 'mensaje2')
--- TP2/Personas.py
+++ TP2/Personas.py
@@ -5,6 +5,48 @@ nombre de pila y dos apellidos) y el número del DNI. Debe ser posible imprimir 
 devolver el nombre o el DNI independientemente.
 
 """
-#
-# class Persona:
-#
+
+
+class Persona:
+    def __init__(self, nombre, apellido1, apellido2, dni):
+        self.__nombre = nombre
+        self.__apellido1 = apellido1
+        self.__apellido2 = apellido2
+        self.__dni = dni
+
+        self.padre = None
+        self.madre = None
+
+    @property
+    def nombre_completo(self):
+        return self.__nombre + ' ' + self.__apellido1 + ' ' + self.__apellido2
+
+    @property
+    def dni(self):
+        return self.__dni
+
+    def __str__(self):
+        return f'Nombre completo: {self.nombre_completo}.\n' \
+               f'DNI: {self.dni}\n' \
+               f'{"" if self.padre is None else "Padre: " + self.padre.nombre_completo}\n' \
+               f'{"" if self.madre is None else "Madre: " + self.madre.nombre_completo}'
+
+    def asignar_padres(self, padre, madre):
+        self.padre = padre
+        self.madre = madre
+
+
+"""
+5. Vínculos
+Modifique el ejemplo anterior para poder construir un árbol genealógico donde se establezca dinámicamente un vínculo que
+ indique qué persona es el padre y cual la madre de una persona dada.
+
+"""
+
+if __name__ == "__main__":
+    franco = Persona('Franco', 'Lopez', 'Alaniz', 756765)
+    papa = Persona('Mos', 'f3', 'qwee', 34634)
+    mama = Persona('Mara', 'asdas', 'Conasdasta', 2356)
+    franco.asignar_padres(papa, mama)
+
+    print(franco)
